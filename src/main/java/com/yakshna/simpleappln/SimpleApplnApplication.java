e2ac995d6f75201a722ceea65b3717c0e388a38a
YT-Arulkumar <arulkumarj@yakshnatechnologies.in>
2023-08-03 11:28:04 +0530

    simple appln main branch push

---
+++ src/main/java/com/yakshna/simpleappln/SimpleApplnApplication.java
@@ -0,0 +1,13 @@
+package com.yakshna.simpleappln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApplnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakshna/SimpleApplnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakshna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
